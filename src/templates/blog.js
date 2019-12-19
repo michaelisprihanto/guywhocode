28c1266b34abb97d55eb8f18bbfe3658ad29deb2
Michael Isprihanto <michael.isprihanto@gmail.com>
2019-12-19 21:17:03 +0700

    Finished blogs using markdown

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Jumbotron from '../components/jumbotron'
+import { graphql } from 'gatsby'
 
-const Blog = () => {
+export const query = graphql`
+query (
+  $slug: String!
+){
+  markdownRemark (
+    fields: {
+      slug: {
+        eq: $slug
+      }
+    }
+  ) {
+    frontmatter {
+      title
+      date
+    }
+    html
+  }
+}
+`
+
+const Blog = (props) => {
   return (
     <Layout>
-      This is a blog template
+      <Jumbotron jumbotronTitle={props.data.markdownRemark.frontmatter.title} jumbotronContent={props.data.markdownRemark.frontmatter.date} />
+      <div className="container">
+      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+      </div>
     </Layout>
   )
 }
