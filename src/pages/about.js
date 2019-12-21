2ec7839f45e5abaff28c89d29158d4fa9cceaa38
Michael Isprihanto <michael.isprihanto@gmail.com>
2019-12-21 21:50:22 +0700

    Finished about

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,40 @@ const AboutPage = () => {
     <Layout>
       <Jumbotron jumbotronTitle="I'm GuyWhoCode" jumbotronContent="About me." />
       <div className="container container-padding">
-        <p>My name is Michael a.k.a. GuyWhoCode, my passion is building things. I have tasted different career journeys in non-tech industries, however my heart has always been into technology. I built this website as a reminder and logs of my journey back into tech industry, and I want to give back to tech communities who have been very supportive of my journeys.</p>
-        <p>DRAFT: i'm the type of person to, bucketlist, elsewhere(contacts), learningtype</p>
+        <p>My name is Michael a.k.a. GuyWhoCode, my passion is building things. I have tasted different career journeys in non-tech industries, however my heart has always been into technology. I built this website as reminders and logs of my journey back into tech industry, and I want to give back to tech communities who have been very supportive of my journeys.</p>
+        <hr />
+        <p><strong>Languages:</strong></p>
+        <ul>
+          <li>Essentials: HTML, CSS</li>
+          <li>Can't live without: JavaScript</li>
+          <li>Most enjoyable to use: React, Python</li>
+        </ul>
+        <hr />
+        <p><strong>This is what type of person i am:</strong></p>
+        <ul>
+          <li>Have thirsts for knowledge</li>
+          <li>A student of the game of basketball</li>
+          <li>Addicted to mangoes</li>
+          <li>Forget non-essential things easily</li>
+        </ul>
+        <hr />
+        <p><strong>My bucketlist includes:</strong></p>
+        <ul>
+          <li>Open a restaurant, free food for people in need</li>
+          <li>Speak at conferences</li>
+          <li>Travel around the world for 2 months</li>
+          <li>Have a company that empowers employees to be useful in communities</li>
+          <li>Able to cook restaurant-grade quality food!</li>
+        </ul>
+        <hr />
+        <p><strong>Elsewhere:</strong></p>
+        <ul>
+          <li>Medium</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+          <li>YouTube</li>
+          <li>GitHub</li>
+        </ul>
       </div>
     </Layout>
   )
